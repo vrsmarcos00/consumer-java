e842a93794fe8293375172ebadd1e86e52255067
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-06 00:09:42 -0300

    Implementation of the interface

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		//A partir de uma lista de proudutos, aumenta o preço dos
+		//produtos em 10%
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		list.forEach(new PriceUpdate());
+		
+		list.forEach(System.out::println);
+
+	}
+
+}
