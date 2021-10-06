f1b791b5c71a8fa94b3e4f35002f9f77f0906f20
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-06 00:23:10 -0300

    Lambda expression inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -23,9 +22,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Consumer<Product> cons = p -> p.setPrice(p.getPrice() + 1.1);
-		
-		list.forEach(cons);
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 		
 		list.forEach(System.out::println);
 
